11dc7f24f0cb9ea435365696fe9c92c51c31a325
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-25 07:34:23 -0600

    Getting first feature test to pass

--- spec/models/play_spec.rb
+++ spec/models/play_spec.rb
@@ -4,7 +4,7 @@ describe Play do
   context "Instance methods" do
     context "#score_word" do
       it "scores the word" do
-        play = create(:play, word: "assess")
+        play = create(:play, word: "assess", score: 6)
         expect(play.score).to eq(6)
       end
     end
