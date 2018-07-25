e88c3e262a55b3820af38f9ef933312837ff68f0
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-25 11:51:44 -0600

    Removes unneeded Suggestion class from inline SentencesController

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -11,19 +11,3 @@ class SentencesController < ApplicationController
     @suggestions = suggestions.suggested_sentences
   end
 end
-
-# class Suggestions
-#   def initialize(response_data)
-#     @data = response_data
-#   end
-
-#   def suggested_sentences
-#     filtered_data = @data['results'][0]['lexicalEntries'][0]['sentences'].select do |sentence_data|
-#       sentence_data['regions'].include?('North American')
-#     end
-    
-#     filtered_data.map do |data|
-#       Sentence.new(data)
-#     end
-#   end
-# end
